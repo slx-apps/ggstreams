7824542e5323f547ddd31bfde2c47f7acc7878f9
Alex Neeky <alek1niki@gmail.com>
2020-10-08 13:34:38 +0300

    cleanup StreamListViewModel

--- app/src/main/java/com/nlx/ggstreams/ui/list/StreamListViewModel.kt
+++ app/src/main/java/com/nlx/ggstreams/ui/list/StreamListViewModel.kt
@@ -4,11 +4,13 @@ import android.util.Log
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.*
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.nlx.ggstreams.models.GGStream
 import com.nlx.ggstreams.ui.list.data.StreamRepository
 import kotlinx.coroutines.flow.Flow
-import javax.inject.Inject
 
 class StreamListViewModel @ViewModelInject constructor(
         private val streamRepository: StreamRepository
@@ -30,12 +32,10 @@ class StreamListViewModel @ViewModelInject constructor(
     fun listLiveData(): Flow<PagingData<GGStream>> = flow
 
     fun invalidateList() {
-        Log.d(TAG, "invalidateList: ")
         ds.invalidate()
     }
 
     private fun createPagingFlow() {
-        Log.d(TAG, "createPagingFlow: ")
         pager = Pager(PagingConfig(pageSize = PAGING_LIMIT)) {
             StreamsPageKeyedDataSource(streamRepository).also {
                ds = it
@@ -43,21 +43,6 @@ class StreamListViewModel @ViewModelInject constructor(
         }
         flow = pager.flow
         .cachedIn(viewModelScope)
-    }
-
-    /*
-     @Inject constructor(val streamRepository: StreamRepository
-    )
-     */
-    inner class StreamsDataSourceFactory  {//
-
-        lateinit var dataSource: StreamsPageKeyedDataSource
-
-        fun create(): PagingSource<String, GGStream> {
-            dataSource = StreamsPageKeyedDataSource(streamRepository)
-            return dataSource
-        }
-
     }
 }
 
--- app/src/main/java/com/nlx/ggstreams/ui/list/StreamsPageKeyedDataSource.kt
+++ app/src/main/java/com/nlx/ggstreams/ui/list/StreamsPageKeyedDataSource.kt
@@ -9,46 +9,6 @@ class StreamsPageKeyedDataSource @Inject constructor(
         private val streamRepository: StreamRepository
 ): PagingSource<String, GGStream>() {
 
-//    override fun loadBefore(
-//            params: LoadParams<String>,
-//            callback: LoadCallback<String, GGStream>) {
-//        // ignored, since we only ever append to our initial load
-//    }
-//
-//    override fun loadAfter(params: LoadParams<String>, callback: LoadCallback<String, GGStream>) {
-//        Log.d(StreamListViewModel.TAG, "loadAfter: " + params.key.toInt())
-//
-//        val response = streamRepository.fetchStreamsSingle(params.key.toInt())
-//
-//        if (response.isSuccessful) {
-//            val res = response.body()
-//            if (res != null) {
-//                if (res.page == res.pageSize) {
-//                    callback.onResult(res.streams, null)
-//                } else {
-//                    callback.onResult(res.streams, (res.page + 1).toString())
-//                }
-//            }
-//        }
-//
-//    }
-//
-//    override fun loadInitial(params: LoadInitialParams<String>,
-//                             callback: LoadInitialCallback<String, GGStream>) {
-//        val response = streamRepository.fetchStreamsSingle(1).execute()
-//
-//        if (response.isSuccessful) {
-//            val res = response.body()
-//            if (res != null) {
-//                if (res.page == res.pageSize) {
-//                    callback.onResult(res.streams, null, null)
-//                } else {
-//                    callback.onResult(res.streams, null, (res.page + 1).toString())
-//                }
-//            }
-//        }
-//    }
-
     override suspend fun load(params: LoadParams<String>): LoadResult<String, GGStream> {
         return try {
             val nextPageNumber = params.key?.toIntOrNull() ?: 1
